f374d172a75bc96fb8855359025722a94b74036d
Daan Leijen <daan@microsoft.com>
2021-04-01 17:35:40 -0700

    compile libraries as one file for better codegen

---
+++ src/mprompt/libmprompt.c
@@ -0,0 +1,11 @@
+/* ----------------------------------------------------------------------------
+  Copyright (c) 2021, Microsoft Research, Daan Leijen
+  This is free software; you can redistribute it and/or modify it
+  under the terms of the MIT License. A copy of the license can be
+  found in the "LICENSE" file at the root of this distribution.
+
+  Include all sources in one file for compilation for better optimization
+-----------------------------------------------------------------------------*/
+#include "util.c"
+#include "gstack.c"
+#include "mprompt.c"
